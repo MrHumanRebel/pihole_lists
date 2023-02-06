73b4f593c31a6dadf22be06ca99c8ef04374c8af
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2023-02-06 23:35:51 +0100

    Update questionable_sources.py

--- .github/scripts/questionable_sources.py
+++ .github/scripts/questionable_sources.py
@@ -3,31 +3,17 @@ import warnings
 from bs4 import BeautifulSoup
 
 url = 'https://mediabiasfactcheck.com/fake-news/'
-texts = []
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     response = requests.get(url, verify=False)
 
+response = requests.get(url)
+
 soup = BeautifulSoup(response.text, 'html.parser')
 
-for link in soup.find_all('a'):
-    if "www." in link.text:
-        texts.append(link.text.replace("www.", "").replace("https://", ""))
-
-cleaned_texts = []
-for text in texts:
-    try:
-        index_slash = text.index("/")
-        cleaned_text = text[:index_slash]
-    except ValueError:
-        cleaned_text = text
-    try:
-        index_bracket = cleaned_text.index("(")
-        cleaned_text = cleaned_text[index_bracket+1:]
-    except ValueError:
-        cleaned_texts.append(cleaned_text.strip())
-
-with open("texts.txt", "w") as file:
-    for text in cleaned_texts:
-        file.write(text + "\n")
+with open('processed_link_names.txt', 'w') as file:
+    for link in soup.find_all('a'):
+        if '(' in link.text:
+            link_name = link.text.split('(')[-1].replace(')', '').replace('www.', '').lower()
+            file.write(link_name + '\n')
