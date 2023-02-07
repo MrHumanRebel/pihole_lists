f0eecb3a689a8cc1593299bc25351583bbbb78ea
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2023-02-07 10:46:17 +0100

    Update wrapper.py

--- .github/scripts/wrapper.py
+++ .github/scripts/wrapper.py
@@ -1,10 +1,25 @@
 import requests
 
-with open("temp/urls.txt") as file:
-    urls = [line.strip() for line in file]
-
-
-with open("complete_blocklist.txt", "w") as outfile:
+def get_addresses_to_block(urls):
+    addresses_to_block = set()
     for url in urls:
-        response = requests.get(url)
-        outfile.write(response.text)
+        lines = process_url(url)
+        [addresses_to_block.add(line) for line in lines]
+        
+    return addresses_to_block
+        
+def process_url(url):
+    response = requests.get(url)
+    
+    lines = response.text.split("\n")
+    return lines
+
+
+def main():
+    with open("temp/urls.txt") as file:
+        urls = [line.strip() for line in file]
+    
+    addresses_to_block = get_addresses_to_block(urls)
+    
+    with open("complete_blocklist.txt", "w") as outfile:
+        outfile.write(addresses_to_block)
