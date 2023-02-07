7c57623874b51a606ec22e2bc67842688d20f505
MrHumanRebel <szekelydani5g@gmail.com>
2023-02-07 11:27:00 +0100

    fix

--- .github/scripts/collector.py
+++ .github/scripts/collector.py
@@ -6,10 +6,12 @@ with open("docs/urls.txt") as file:
     urls = [line.strip() for line in file]
 
 
+parent_dir = "external"
+if not os.path.exists(parent_dir):
+    os.makedirs(parent_dir)
+
 for i, url in enumerate(urls):
-    filename = f"external/{i+1}_{url.split('/')[-1]}.txt"
-    with open(filename, "w+") as f:
-        for url in urls:
-            response = requests.get(url)
-            with open(url.split("/")[-1], "w") as f:
-                f.write(response.text)
+    filename = f"{parent_dir}/{i+1}_{url.split('/')[-1]}"
+    with open(filename, "w") as f:
+        response = requests.get(url)
+        f.write(response.text)
