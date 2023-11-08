57ce02ed4cae86e214b3c988706b3b1d5ac95725
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2023-11-08 19:26:02 +0100

    Update jogsertowebaruhazak.py

--- .github/scripts/jogsertowebaruhazak.py
+++ .github/scripts/jogsertowebaruhazak.py
@@ -1,16 +1,28 @@
 import requests
+from selenium import webdriver
 
 def extract_urls_from_server_response(base_url, page_start, file_name):
     page_end = None  # Initialize page_end as None
+
+    def scrape_website(url):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+
+        try:
+            driver.get(url)
+            # Extract data from the loaded webpage using Selenium
+
+            # Close the browser
+            driver.quit()
+        except Exception as e:
+            print(f"An error occurred while scraping the website: {str(e)}")
 
     while page_end is None or page_start <= page_end:
         url = f"{base_url}&page={page_start}"
         
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
         response = requests.get(url, headers=headers)
-
-
-        response = requests.get(url)
 
         if response.status_code == 200:
             data = response.json()
@@ -33,6 +45,9 @@ def extract_urls_from_server_response(base_url, page_start, file_name):
             with open(file_name, "a") as file:
                 for text in texts:
                     file.write(text + "\n")
+            
+            # Call the web scraping function for this URL
+            scrape_website(url)
         else:
             print(f"Failed to retrieve data from the server. Status code: {response.status_code}")
 
